1d2313939e606f42d97dd73ca1614e86a59d4d8e
aravinthan97 <26415575+aravinthan97@users.noreply.github.com>
2018-06-19 02:55:56 +0800

    adminNew look
    new look

--- adminindex.php
+++ adminindex.php
@@ -19,23 +19,17 @@
 
        {
             echo '
-                 <tr>
-                      <td>
-                           <h2>'.($row['CompanyName']).'</h2>
-                           <img src="data:image/jpeg;base64,'.base64_encode($row['Logo'] ).'" height="200" width="200" class="img-thumnail" />
-                           
-
-                      </td>
-                 </tr>
+            <div class="col-md-4">
+                <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
+                <img src="data:image/jpeg;base64,'.base64_encode($row['Logo'] ).'" class="img-responsive"  height="200" width="200"/><br/>
+                <h4 class="text-info">'.($row['CompanyName']).'</h4>
+              </div>
+           </div>
             ';
        }
 
 
           ?>
-
-
-
-
   </body>
 </html>
 <script type='text/javascript' src='js\image.js'></script>
--- includes/admin_nav.php
+++ includes/admin_nav.php
@@ -5,8 +5,26 @@ session_start();
 <html>
   <body>
     <?php
+  $name=  $_SESSION['AdminName'];
     if (isset($_SESSION['AdminID'])) {
-      echo '  <div class="topnav">
+      echo '<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="adminindex.php">'.$name.'</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="adminindex.php">Home</a></li>
+      <li><a href="create_shop.php">create shop</a></li>
+        <li><a href="additeam.php">add iteam</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+    <form action="includes\logout_int.php" method="post">
+      <button type="submit" class="glyphicon glyphicon-log-in" name="logout">logout</button>
+    </form>
+    </ul>
+  </div>
+</nav>';
+/*      echo '  <div class="topnav">
         <a class="active1" href="adminindex.php">Zedtext</a>
         <a href="create_shop.php">create shop</a>
         <a href="additeam.php">add iteam</a>
@@ -15,7 +33,7 @@ session_start();
         </form>
         </div>
       </div>';
-      echo $_SESSION['AdminName'];
+      echo $_SESSION['AdminName']; */
     }
 
     ?>
